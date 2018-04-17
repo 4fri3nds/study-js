66bc5822ad385491f0ab41da517fc5b9b16acb6e
Alex <Alex@MacBook-Air-Alex.local>
2018-04-17 19:31:27 +0300

    second commit

--- js/les9.js
+++ js/les9.js
@@ -1,19 +1,39 @@
 // 1 task
-let checkBtn = document.getElementById('check-switch');
 let checkBox = document.getElementById('check-box');
+let checkBtn = document.getElementById('check-switch');
 
-checkBtn.addEventListener('click', checkOn);
+checkBtn.addEventListener('click', triger1);
 
-function checkOn() {
-  checkBox.setAttribute('checked', 'checked');
-  this.addEventListener('click', checkOff);
-  this.removeEventListener('click', checkOn);
+function triger1() {
+  attrCheck(checkBox);
 }
-function checkOff() {
-  checkBox.removeAttribute('checked', 'checked');
-  this.removeEventListener('click', checkOff);
-  this.addEventListener('click', checkOn);
+
+function attrCheck(elem) {
+  if (!elem.hasAttribute('checked')){
+    elem.setAttribute('checked', 'checked');
+  } else {
+    elem.removeAttribute('checked', 'checked');
+  }
 }
+
+// function checkOn() {
+//   this.addEventListener('click', checkOff);
+//   this.removeEventListener('click', checkOn);
+// }
+
+// function checkOff() {
+//   this.removeEventListener('click', checkOff);
+//   this.addEventListener('click', checkOn);
+// }
 
 // 2 task
+let checkBox2 = document.getElementsByClassName('check-box');
+let checkBtn2 = document.getElementById('check-switch2');
 
+checkBtn2.addEventListener('click', checkOnAll);
+
+function checkOnAll() {
+  for (let x = 0; x < checkBox2.length; x ++) {
+    attrCheck(checkBox2[x])
+  }
+}
